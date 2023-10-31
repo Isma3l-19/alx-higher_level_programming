37ada0e33a8e0747fd69ba96e9bbdd5a4fc3e5dc
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-10-31 07:49:10 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """"printing lowercase alphabets"""
-for letters in range(97, 123)
-print("{}".format(chr(letters, end=""))
+for letters in range(97, 123):
+    print("{}".format(chr(letters, end=""))
