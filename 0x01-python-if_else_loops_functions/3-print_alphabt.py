316ebca94f5e7dcc44eb3f872d5cbbfbfdab128a
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-10-31 11:35:06 -0700

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 """Program that prints alphabets in lowercase"""
 for letters in range(97, 123):
     if letters != 101 and letters != 113:
-        print(f"chr({letters}))
+        print("{}".format(chr(letters)), end="")
