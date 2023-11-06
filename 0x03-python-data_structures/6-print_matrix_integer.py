1b72a975d6f21015aafe7feeb18713d0a57bc44f
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-06 02:44:34 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrics of integers"""
+    """Print a matrix of integers."""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
