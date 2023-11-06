237bb2a54410b3806ae80cc406b45d1c559203e9
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-06 02:35:50 -0800

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Replaces elements in a list"""
 def new_in_list(my_list, idx, element):
+    """Replaces elements in a list"""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
     copy = [x for x in my_list]
