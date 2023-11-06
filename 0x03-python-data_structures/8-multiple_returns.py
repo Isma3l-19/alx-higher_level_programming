f98bbae3d8cb94662fc20ba9ba6d1679db72d2df
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-06 04:51:17 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Returns a tuple with the length of a string"""
-def multiple_return(sentence):
-     if sentence == "":
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character"""
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
