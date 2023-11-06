faa7ab112248f1cc611503d76c2aaed15ed5985b
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-06 02:32:03 -0800

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Replace elements in a list like c"""
 def replace_in_list(my_list, idx, element):
+    """Replace elements in a list like c"""
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
