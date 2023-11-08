f413e9842411568581e67ada8625cadd5bd8afb6
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-08 09:33:47 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,10 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    tom = list(a_dictionary.keys())[0]
-    boy = a_dictionary[tom]
-    for i, j in a_dictionary.items():
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
         if v > big:
-            boy = j
-            tom = i
-    return (tom)
+            big = v
+            ret = k
+    return (ret)
