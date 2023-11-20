a0afd995701eb658c44c565ddcbeddffa877496e
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-11-20 06:32:43 -0800

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    """divides two integers"""
+    """Returns the division of a by b"""
     try:
-        quotient = a / b
+        div = a / b
     except (TypeError, ZeroDivisionError):
-        quotient = None
+        div = None
     finally:
         print("Inside result: {}".format(div))
-    return (quoteint)
+    return (div)
