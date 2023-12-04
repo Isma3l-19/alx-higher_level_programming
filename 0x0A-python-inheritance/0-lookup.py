617518ba43975ea79cbe5a0bd0060ce0f4764d7c
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-12-04 05:17:37 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+def lookup(obj):
+    """returns list of attributes and methods"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+0x0A. Python - Inheritance - its more about how to work around inheritance
