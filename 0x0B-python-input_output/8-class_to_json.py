f7606e3575c9e864b271e01079a5b36e2a765641
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-12-05 10:55:07 -0800

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""defines a Python class-to-JSON function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary represntation of a simple data structure"""
+    return obj.__dict__
