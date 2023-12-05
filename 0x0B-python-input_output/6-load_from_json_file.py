50f7b9069807025d8a71dac215ac53d9df5e86d3
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-12-05 10:47:56 -0800

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""defines a JSON file-reading function"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates a Python object from a JSON file"""
+    with open(filename) as f:
+        return json.load(f)
