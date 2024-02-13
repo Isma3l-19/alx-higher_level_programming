0b146cee26dbd2a1bd357de0881fd0f67ebeeee8
Isma3l-19 <ismaelnyambu.99@gmail.com>
2024-02-13 10:50:20 -0800

    Mandatory

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square.js');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    if (!this.width || !this.height) {
+      console.log('Invalid dimensions for the square.');
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+};
