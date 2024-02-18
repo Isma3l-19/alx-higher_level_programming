9a5d6fe4659d8ddd6e1a63cece4159024061c8b1
Isma3l-19 <ismaelnyambu.99@gmail.com>
2024-02-18 08:54:44 -0800

    Mandatory

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that defines a city class
+"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    city class module
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
