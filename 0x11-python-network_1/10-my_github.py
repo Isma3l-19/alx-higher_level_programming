9f70bdf684ffaad361402e57da8ba141304ec5b9
Isma3l-19 <133232846+Isma3l-19@users.noreply.github.com>
2024-03-01 20:32:52 +0000

    Mandatory

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+takes your github credentials(username, password)
+it uses github API to display your id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print(user_info.get("id"))
+    else:
+        print("Not a valid JSON")
